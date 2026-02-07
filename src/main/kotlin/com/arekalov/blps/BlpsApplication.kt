f2be4bb6ad013c24cb8e981219c75150bece3596
arekalov <artyom.rekalov@gmail.com>
2026-02-07 16:07:10 +0300

    [feat] initial spring boot project

---
+++ src/main/kotlin/com/arekalov/blps/BlpsApplication.kt
@@ -0,0 +1,11 @@
+package com.arekalov.blps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlpsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlpsApplication>(*args)
+}
---
+++ src/test/kotlin/com/arekalov/blps/BlpsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arekalov.blps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlpsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
